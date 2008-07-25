ed15af3de1c1f57856c1463010801dd69d0a0449
Michael Nutt <mnutt@limespot.com>
2008-07-25 16:34:36 -0400

    Re-route home to /all for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def home
-    @podcasts = Podcast.find(:all, :limit => 20, :order => "created_at DESC")
-    @episodes = Episode.find(:all, :limit => 20, :order => "published_at DESC")
-    render :text => "Coming soon."
+    # @podcasts = Podcast.find(:all, :limit => 20, :order => "created_at DESC")
+    # @episodes = Episode.find(:all, :limit => 20, :order => "published_at DESC")
+    redirect_to '/all'
   end
 end

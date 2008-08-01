9986886d62e0a6c514dcc0f3c889add43e901a77
Michael Nutt <mnutt@limespot.com>
2008-08-01 12:54:59 -0400

    Adds podcast pending specs

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -3,18 +3,32 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Podcast do
   before(:each) do
     @podcast = Podcast.new
+    @podcast.title = "My Podcast"
   end
 
   it "should be valid" do
     @podcast.should be_valid
   end
 
-  it 'should have a logo'
-  it 'should be taggable'
-  it 'should have a param with the name in it'
+  it 'should have a logo' do
+    @file = PaperClipFile.new
+    @podcast.attachment_for(:logo).assign(@file)
+    @podcast.logo.should_not be_nil
+  end
+
+  it 'should be taggable' do
+    @podcast.tag_list = "hi"
+    @podcast.save
+    @podcast.tags.size.should == 1
+  end
+
+  it 'should have a param with the name in it' do
+    @podcast.save
+    @podcast.to_param.should == "My-Podcast"
+  end
 end
 
-describe Podcast, "creating a new podcast from feed" do
+describe Podcast, "creating a new podcast" do
   it 'should set the feed url'
   it 'should extract the title'
   it 'should extract the site link'
@@ -23,7 +37,42 @@ describe Podcast, "creating a new podcast from feed" do
   it 'should extract the language'
 end
 
-describe Podcast, "saving a podcast" do
-  it 'should retrieve episodes from the feed'
-  it 'should download the logo if a link is provided'
+describe Podcast, "creating a new podcast when the user is not the feed owner" do
+  it 'should set the user as the finder'
+  it 'should not set the user as the owner'
+end
+
+describe Podcast, "creating a new podcast when the user is the feed owner" do
+  it 'should set the user as the finder'
+  it 'should set the user as the owner'
+end
+
+describe Podcast, "creating a new podcast when the user is not logged in" do
+  it 'should not set the user as the finder'
+  it 'should not set the user as the owner'
+end
+
+describe Podcast, "creating a new podcast with a non-existant URL" do
+  it 'should raise an error that the URL is not contactable'
+  it 'should not save the podcast'
+end
+
+describe Podcast, "creating a new podcast with an RSS feed that is not a podcast" do
+  it 'should raise an error that the feed is not a podcast'
+  it 'should not save the podcast'
+end
+
+describe Podcast, "creating a new podcast with a non-URL string" do
+  it 'should raise an error that the feed is not a URL'
+  it 'should not save the podcast'
+end
+
+describe Podcast, "creating a new podcast when a weird server error occurs" do
+  it 'should raise an error that an unknown exception occurred'
+  it 'should not save the podcast'
+end
+
+describe Podcast, "creating a new podcast that already exists in the system" do
+  it 'should raise an error that the podcast has already been registered'
+  it 'should not save the podcast again'
 end

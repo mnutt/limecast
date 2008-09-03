4feb4e37fec2fef708d395ffd8809656252cb72f
Justin Camerer <jcamerer@limewire.com>
2008-09-03 17:57:26 -0400

    Integrated new duration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def time_to_words(time)
-    if time < 1.minute then
-      return "#{time} sec"
-    elsif time < 10.minutes  then
-      return "#{(time / 1.minute)} min #{(time % 1.minute)} sec"
-    elsif time < 1.hour  then
-      return "#{(time / 1.minute)} min"
-    elsif time < 1.day then
-      return "#{(time / 1.hour)} hr #{(time % 1.hour / 1.minute)} min"
-    elsif time < 7.days then
-      return "#{(time / 1.day)} day #{(time % 1.day / 1.hour)} hr"
-    else # more than 7 days
-      return "#{(time / 1.day)} day"
-    end
+    time.to_duration
   end
 
   def link_to_profile(user)
@@ -27,19 +15,5 @@ module ApplicationHelper
   def relative_time(date)
     time_ago = Time.now - date
     date_to_words(time_ago) + " ago"
-  end
-
-  def pretty_duration(seconds)
-    seconds = seconds.to_i
-    pretty_time_array = []
-    if seconds > 60*60
-      pretty_time_array << (seconds / (60 * 60)).to_s + " hr"
-      seconds = seconds % (60 * 60)
-    elsif seconds > 60
-      pretty_time_array << (seconds / 60).to_s + " min"
-      seconds = seconds % 60
-    end
-    pretty_time_array << seconds.to_s + " sec"
-    pretty_time_array.join(" ")
   end
 end

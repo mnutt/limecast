b915af38fb8231133cb2a8358a1d06080c271cac
Justin Camerer <jcamerer@limewire.com>
2008-09-03 17:42:33 -0400

    Added string_spec

---
+++ spec/lib/string_spec.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe String do
+
+  describe "#to_url" do
+    it 'should remove the protocol from a url' do
+      "http://google.com".to_url.should == "google.com"
+    end
+
+    it 'should remove trailing slashes' do
+      "google.com/".to_url.should == "google.com"
+    end
+
+    it 'should remove the "www."' do
+      "www.google.com".to_url.should == "google.com"
+    end
+
+    it 'should leave just the domain and tld of "http://www.google.com/"' do
+      "http://www.google.com/".to_url.should == "google.com"
+    end
+
+    it 'should leave the subdomain of "mail.google.com"' do
+      "http://mail.google.com/".to_url.should == "mail.google.com"
+    end
+  end
+
+end
+

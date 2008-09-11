e672735beef0fe3bce0261cc066890c46de2e956
Justin Camerer <iamjwc@gmail.com>
2008-09-11 17:36:31 -0400

    Added show_add_form? helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,8 @@
 module CommentsHelper
+  def show_add_form?(commentable)
+    !commentable.been_reviewed_by?(current_user)
+  end
+
   def show_edit_link?(comment)
     comment.editable? && (comment.commenter.nil? || comment.commenter == current_user)
   end

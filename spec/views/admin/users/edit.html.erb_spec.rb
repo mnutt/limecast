498f16368e1ec16f5d1142f7dc0d7b79cbd6efad
Justin Camerer <iamjwc@gmail.com>
2008-09-11 11:23:54 -0400

    Commented out some old specs

--- spec/views/admin/users/edit.html.erb_spec.rb
+++ spec/views/admin/users/edit.html.erb_spec.rb
@@ -1,17 +1,17 @@
-require File.dirname(__FILE__) + '/../../../spec_helper'
-
-describe "/admin/users/edit.html.erb" do
-  before do
-    @user = mock_model(User)
-    assigns[:user] = @user
-  end
-
-  it "should render edit form" do
-    render "/admin/users/edit.html.erb"
-
-    response.should have_tag("form[action=#{admin_user_path(@user)}][method=post]") do
-    end
-  end
-end
-
-
+# require File.dirname(__FILE__) + '/../../../spec_helper'
+# 
+# describe "/admin/users/edit.html.erb" do
+#   before do
+#     @user = mock_model(User)
+#     assigns[:user] = @user
+#   end
+# 
+#   it "should render edit form" do
+#     render "/admin/users/edit.html.erb"
+# 
+#     response.should have_tag("form[action=#{admin_user_path(@user)}][method=post]") do
+#     end
+#   end
+# end
+# 
+# 
--- spec/views/admin/users/new.html.erb_spec.rb
+++ spec/views/admin/users/new.html.erb_spec.rb
@@ -1,18 +1,18 @@
-require File.dirname(__FILE__) + '/../../../spec_helper'
-
-describe "/admin_users/new.html.erb" do
-  before(:each) do
-    @user = mock_model(User)
-    @user.stub!(:new_record?).and_return(true)
-    assigns[:user] = @user
-  end
-
-  it "should render new form" do
-    render "/admin/users/new.html.erb"
-
-    response.should have_tag("form[action=?][method=post]", admin_users_path) do
-    end
-  end
-end
-
-
+# require File.dirname(__FILE__) + '/../../../spec_helper'
+# 
+# describe "/admin_users/new.html.erb" do
+#   before(:each) do
+#     @user = mock_model(User)
+#     @user.stub!(:new_record?).and_return(true)
+#     assigns[:user] = @user
+#   end
+# 
+#   it "should render new form" do
+#     render "/admin/users/new.html.erb"
+# 
+#     response.should have_tag("form[action=?][method=post]", admin_users_path) do
+#     end
+#   end
+# end
+# 
+# 

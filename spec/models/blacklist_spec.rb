a36376b01a32282b1c54c74e4ee8054d766adc8f
Justin Camerer <iamjwc@gmail.com>
2008-09-12 17:33:13 -0400

    Fixed blacklist spec

--- spec/models/blacklist_spec.rb
+++ spec/models/blacklist_spec.rb
@@ -1,13 +1,13 @@
-# require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-# 
-# describe Blacklist do
-#   before(:each) do
-#     @valid_attributes = {
-#       :domain => "value for domain"
-#     }
-#   end
-# 
-#   it "should create a new instance given valid attributes" do
-#     Blacklist.create!(@valid_attributes)
-#   end
-# end
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Blacklist do
+  before(:each) do
+    @valid_attributes = {
+      :domain => "value for domain"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Blacklist.create!(@valid_attributes)
+  end
+end

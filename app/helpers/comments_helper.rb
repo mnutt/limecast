31eacd6e4c58c1d4cbd6a46090f0f74504be4991
Justin Camerer <iamjwc@gmail.com>
2008-09-18 16:32:03 -0400

    Removed useless helper methods

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,2 @@
 module CommentsHelper
-  def show_add_comment?(commentable)
-    !commentable.been_reviewed_by?(current_user)
-  end
-
-  def show_modify_comment?(comment)
-    comment.editable? && (comment.commenter.nil? || comment.commenter == current_user)
-  end
 end

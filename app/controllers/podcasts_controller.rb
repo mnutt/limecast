4211908ba2f5f15463245dbacaab8b6671d639f5
Justin Camerer <iamjwc@gmail.com>
2008-09-24 15:32:22 -0400

    Many more of the podcast controller specs working

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,32 +1,18 @@
 class PodcastsController < ApplicationController
   before_filter :login_required, :only => [:edit, :update, :destroy]
-  # GET /podcasts
-  # GET /podcasts.xml
+
   def index
     @podcasts = Podcast.parsed.find(:all, :order => "title ASC")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @podcasts }
-    end
   end
 
-  # GET /podcasts/1
-  # GET /podcasts/1.xml
   def show
     @podcast = Podcast.find_by_clean_url(params[:podcast]) or raise ActiveRecord::RecordNotFound
     @episodes = @podcast.episodes.find(:all, :limit => 3)
 
     @comments = with(@podcast.episodes.newest.first) {|ep| ep.nil? ? [] : ep.comments }
     @comment  = Comment.new(:episode => @podcast.episodes.newest.first)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @podcast }
-    end
   end
 
-  # GET /search
   def search
     if params[:q]
       redirect_to :controller => 'podcasts', :action => 'search', :query => params[:q]
@@ -42,18 +28,10 @@ class PodcastsController < ApplicationController
     render :partial => "bad_feed"
   end
 
-  # GET /podcasts/new
-  # GET /podcasts/new.xml
   def new
     @new_podcast = Podcast.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @podcast }
-    end
   end
 
-  # GET /podcasts/1/edit
   def edit
     @podcast = Podcast.find(params[:id])
     authorize_write @podcast
@@ -72,8 +50,6 @@ class PodcastsController < ApplicationController
     end
   end
 
-  # POST /podcasts
-  # POST /podcasts.xml
   def create
     @podcast = Podcast.create!(:feed_url => params[:podcast][:feed_url], 
                                :user     => current_user)
@@ -86,36 +62,26 @@ class PodcastsController < ApplicationController
     render :nothing => true
   end
 
-  # PUT /podcasts/1
-  # PUT /podcasts/1.xml
   def update
     @podcast = Podcast.find(params[:id])
     authorize_write @podcast
 
     @podcast.attributes = params[:podcast]
-    respond_to do |format|
-      if @podcast.save
-        flash[:notice] = 'Podcast was successfully updated.'
-        format.html { redirect_to(@podcast) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @podcast.errors, :status => :unprocessable_entity }
-      end
+
+    if @podcast.save
+      flash[:notice] = 'Podcast was successfully updated.'
+      redirect_to(@podcast)
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /podcasts/1
-  # DELETE /podcasts/1.xml
   def destroy
     @podcast = Podcast.find(params[:id])
     authorize_write @podcast
 
     @podcast.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(podcasts_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(podcasts_url)
   end
 end
--- spec/controllers/podcasts_controller_spec.rb
+++ spec/controllers/podcasts_controller_spec.rb
@@ -1,127 +1,57 @@
-# require File.dirname(__FILE__) + '/../spec_helper'
-# 
-# describe PodcastsController do
-#   describe "handling GET /podcasts" do
-# 
-#     before(:each) do
-#       @podcast = mock_model(Podcast)
-#       Podcast.stub!(:find).and_return([@podcast])
-#     end
-#   
-#     def do_get
-#       get :index
-#     end
-#   
-#     it "should be successful" do
-#       do_get
-#       response.should be_success
-#     end
-# 
-#     it "should render index template" do
-#       do_get
-#       response.should render_template('index')
-#     end
-#   
-#     it "should find all podcasts" do
-#       Podcast.should_receive(:find).with(:all).and_return([@podcast])
-#       do_get
-#     end
-#   
-#     it "should assign the found podcasts for the view" do
-#       do_get
-#       assigns[:podcasts].should == [@podcast]
-#     end
-#   end
-# 
-#   describe "handling GET /podcasts.xml" do
-# 
-#     before(:each) do
-#       @podcasts = mock("Array of Podcasts", :to_xml => "XML")
-#       Podcast.stub!(:find).and_return(@podcasts)
-#     end
-#   
-#     def do_get
-#       @request.env["HTTP_ACCEPT"] = "application/xml"
-#       get :index
-#     end
-#   
-#     it "should be successful" do
-#       do_get
-#       response.should be_success
-#     end
-# 
-#     it "should find all podcasts" do
-#       Podcast.should_receive(:find).with(:all).and_return(@podcasts)
-#       do_get
-#     end
-#   
-#     it "should render the found podcasts as xml" do
-#       @podcasts.should_receive(:to_xml).and_return("XML")
-#       do_get
-#       response.body.should == "XML"
-#     end
-#   end
-# 
-#   describe "handling GET /podcasts/1" do
-# 
-#     before(:each) do
-#       @podcast = mock_model(Podcast)
-#       Podcast.stub!(:find).and_return(@podcast)
-#     end
-#   
-#     def do_get
-#       get :show, :id => "1"
-#     end
-# 
-#     it "should be successful" do
-#       do_get
-#       response.should be_success
-#     end
-#   
-#     it "should render show template" do
-#       do_get
-#       response.should render_template('show')
-#     end
-#   
-#     it "should find the podcast requested" do
-#       Podcast.should_receive(:find).with("1").and_return(@podcast)
-#       do_get
-#     end
-#   
-#     it "should assign the found podcast for the view" do
-#       do_get
-#       assigns[:podcast].should equal(@podcast)
-#     end
-#   end
-# 
-#   describe "handling GET /podcasts/1.xml" do
-# 
-#     before(:each) do
-#       @podcast = mock_model(Podcast, :to_xml => "XML")
-#       Podcast.stub!(:find).and_return(@podcast)
-#     end
-#   
-#     def do_get
-#       @request.env["HTTP_ACCEPT"] = "application/xml"
-#       get :show, :id => "1"
-#     end
-# 
-#     it "should be successful" do
-#       do_get
-#       response.should be_success
-#     end
-#   
-#     it "should find the podcast requested" do
-#       Podcast.should_receive(:find).with("1").and_return(@podcast)
-#       do_get
-#     end
-#   
-#     it "should render the found podcast as xml" do
-#       @podcast.should_receive(:to_xml).and_return("XML")
-#       do_get
-#       response.body.should == "XML"
-#     end
-#   end
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PodcastsController do
+  describe "handling GET /" do
+
+    before(:each) do
+      @podcast = Factory.create(:parsed_podcast)
+    end
+  
+    def do_get
+      get :index
+    end
+  
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+
+    it "should render index template" do
+      do_get
+      response.should render_template('index')
+    end
+  
+    it "should assign the found podcasts for the view" do
+      do_get
+      assigns[:podcasts].should == [@podcast]
+    end
+  end
+
+  describe "handling GET /:podcast" do
+
+    before(:each) do
+      @podcast = Factory.create(:parsed_podcast)
+    end
+  
+    def do_get
+      get :show, :podcast => @podcast.clean_url
+    end
+
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+  
+    it "should render show template" do
+      do_get
+      response.should render_template('show')
+    end
+  
+    it "should assign the found podcast for the view" do
+      do_get
+      assigns[:podcast].id.should equal(@podcast.id)
+    end
+  end
 # 
 #   describe "handling GET /podcasts/new" do
 # 
@@ -233,57 +163,6 @@
 #     end
 #   end
 # 
-#   describe "handling PUT /podcasts/1" do
-# 
-#     before(:each) do
-#       @podcast = mock_model(Podcast, :to_param => "1")
-#       Podcast.stub!(:find).and_return(@podcast)
-#     end
-#     
-#     describe "with successful update" do
-# 
-#       def do_put
-#         @podcast.should_receive(:update_attributes).and_return(true)
-#         put :update, :id => "1"
-#       end
-# 
-#       it "should find the podcast requested" do
-#         Podcast.should_receive(:find).with("1").and_return(@podcast)
-#         do_put
-#       end
-# 
-#       it "should update the found podcast" do
-#         do_put
-#         assigns(:podcast).should equal(@podcast)
-#       end
-# 
-#       it "should assign the found podcast for the view" do
-#         do_put
-#         assigns(:podcast).should equal(@podcast)
-#       end
-# 
-#       it "should redirect to the podcast" do
-#         do_put
-#         response.should redirect_to(podcast_url("1"))
-#       end
-# 
-#     end
-#     
-#     describe "with failed update" do
-# 
-#       def do_put
-#         @podcast.should_receive(:update_attributes).and_return(false)
-#         put :update, :id => "1"
-#       end
-# 
-#       it "should re-render 'edit'" do
-#         do_put
-#         response.should render_template('edit')
-#       end
-# 
-#     end
-#   end
-# 
 #   describe "handling DELETE /podcasts/1" do
 # 
 #     before(:each) do
@@ -310,4 +189,4 @@
 #       response.should redirect_to(podcasts_url)
 #     end
 #   end
-# end
+end

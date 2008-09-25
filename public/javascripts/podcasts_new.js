e5f9ef103b8270574ec351067ab1a856c86dca9f
Justin Camerer <iamjwc@gmail.com>
2008-09-25 14:56:38 -0400

    More formatting changes

--- public/javascripts/podcasts_new.js
+++ public/javascripts/podcasts_new.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
   $('form#new_podcast').submit(function(){
     $.ajax({
@@ -39,48 +37,5 @@ $(document).ready(function(){
     // Keep form from submitting
     return false;
   });
-
-
 });
 
-// 
-//   $.ajax(
-//     {
-//       data:     $.param($(this).serializeArray()) + '&amp;authenticity_token=' + encodeURIComponent('1c4f6c74d275107710bba4b5ca41a0ae28f7d272'),
-//       dataType: 'script',
-//       error:    function(request){new Lime.Widgets.Add.Podcast()},
-//       success:  function(request){new Lime.Widgets.Add.Podcast()},
-//       type:     'post',
-//       url:      '/podcasts'
-//     }
-//   );
-// 
-// 
-// 
-// 
-// 
-// 
-// Lime.Widgets.Add = Class.create();
-// Lime.Widgets.Add.Podcast = Class.create();
-// Object.extend(Lime.Widgets.Add.Podcast, {
-//   list: Array
-// });
-// Object.extend(Lime.Widgets.Add.Podcast.prototype, {
-//   initialize: function() {
-//     feed_url = $('podcast_feed_url').value;
-//     $('new_podcast').reset();
-//     form_html = $('form_clone').innerHTML.replace(/CHANGE/, feed_url);
-// 
-//     status = Builder.node('div', { className: "status" });
-//     form = Builder.node('div', { className: "form" });
-//     form.innerHTML = form_html;
-//     podcast = Builder.node('div', { className: "added_podcast" }, [ form, status ]);
-//     $('added_podcast_list').appendChild(podcast);
-//     this._updater = new Ajax.PeriodicalUpdater(status,
-//                                                '/status/' + encodeURIComponent(feed_url).replace(/%2F/g, '/'),
-//                                                {method: 'get', frequency: 1, decay: 2, stopOnText: "status_message"});
-// 
-// 
-//     if($('inline_login')) { $('inline_login').show(); }
-//   }
-// });

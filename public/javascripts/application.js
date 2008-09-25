bc8bb31ddc024dd74f5747d105a9a451cff88ba0
Justin Camerer <iamjwc@gmail.com>
2008-09-25 14:47:22 -0400

    Lime Podcast Add widget no longer used

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -261,27 +261,27 @@ document.observe('dom:loaded', function() {
 * Add Podcast
 **************************************************************/
 
-Lime.Widgets.Add = Class.create();
-Lime.Widgets.Add.Podcast = Class.create();
-Object.extend(Lime.Widgets.Add.Podcast, {
-  list: Array
-});
-Object.extend(Lime.Widgets.Add.Podcast.prototype, {
-  initialize: function() {
-    feed_url = $('podcast_feed_url').value;
-    $('new_podcast').reset();
-    form_html = $('form_clone').innerHTML.replace(/CHANGE/, feed_url);
-
-    status = Builder.node('div', { className: "status" });
-    form = Builder.node('div', { className: "form" });
-    form.innerHTML = form_html;
-    podcast = Builder.node('div', { className: "added_podcast" }, [ form, status ]);
-    $('added_podcast_list').appendChild(podcast);
-    this._updater = new Ajax.PeriodicalUpdater(status,
-                                               '/status/' + encodeURIComponent(feed_url).replace(/%2F/g, '/'),
-                                               {method: 'get', frequency: 1, decay: 2, stopOnText: "status_message"});
-
-
-    if($('inline_login')) { $('inline_login').show(); }
-  }
-});
+// Lime.Widgets.Add = Class.create();
+// Lime.Widgets.Add.Podcast = Class.create();
+// Object.extend(Lime.Widgets.Add.Podcast, {
+//   list: Array
+// });
+// Object.extend(Lime.Widgets.Add.Podcast.prototype, {
+//   initialize: function() {
+//     feed_url = $('podcast_feed_url').value;
+//     $('new_podcast').reset();
+//     form_html = $('form_clone').innerHTML.replace(/CHANGE/, feed_url);
+// 
+//     status = Builder.node('div', { className: "status" });
+//     form = Builder.node('div', { className: "form" });
+//     form.innerHTML = form_html;
+//     podcast = Builder.node('div', { className: "added_podcast" }, [ form, status ]);
+//     $('added_podcast_list').appendChild(podcast);
+//     this._updater = new Ajax.PeriodicalUpdater(status,
+//                                                '/status/' + encodeURIComponent(feed_url).replace(/%2F/g, '/'),
+//                                                {method: 'get', frequency: 1, decay: 2, stopOnText: "status_message"});
+// 
+// 
+//     if($('inline_login')) { $('inline_login').show(); }
+//   }
+// });

cb5e6e9279d41149723224e70da74f7d2da86150
Justin Camerer <iamjwc@gmail.com>
2008-09-30 16:13:05 -0400

    No longer showing comment field if the user already commented on the page.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,12 @@
 module CommentsHelper
   def can_add_comments?(episode)
-    !episode.been_reviewed_by?(current_user) && episode.open_for_comments?
+    comment_in_session = Comment.find(:all, session.data[:comments]).map(&:episode_id).includes?(episode.id)
+
+    if current_user.nil?
+      !comment_in_session && episode.open_for_comments?
+    else
+      !episode.been_reviewed_by?(current_user) && episode.open_for_comments?
+    end
   end
 
   def can_edit_comment?(comment)

078c94d1ab53880dbc76d8f3fd23cdd0e8ea4a24
Justin Camerer <iamjwc@gmail.com>
2008-10-07 11:39:24 -0400

    Refactored comment deletion using new restful delete plugin

--- public/javascripts/comment_delete.js
+++ public/javascripts/comment_delete.js
@@ -1,24 +1,14 @@
 jQuery(document).ready(function(){
   jQuery('li.review').map(function(){
-    var review_li   = jQuery(this);
-    var delete_link = jQuery(this).find('a.delete');
+    var review_li = jQuery(this);
 
-    // Reveal delete link if they have javascript enabled.
-    delete_link.show();
-
-    delete_link.click(function(){
-      // Remove the review from the db.
-      jQuery.ajax({
-        type: 'post',
-        url:  jQuery(this).attr('href'),
-        data: '_method=delete&authenticity_token=' + AUTH_TOKEN
+    review_li.find('a.delete')
+      .show()
+      .restfulDelete()
+      .click(function(){
+        review_li.replaceWith('');
+        return false;
       });
-
-      // Remove the review from the UI.
-      review_li.replaceWith('');
-
-      return false;
-    });
   });
 });
 

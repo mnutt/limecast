56de35dc7e20cfa5207bb443610883495590da36
Justin Camerer <iamjwc@gmail.com>
2008-10-10 11:59:54 -0400

    Oops, I broke the javascript podcast adder. Fixed now

--- public/javascripts/podcasts_new.js
+++ public/javascripts/podcasts_new.js
@@ -2,13 +2,13 @@ jQuery(document).ready(function(){
   jQuery('form#new_podcast').submit(function(){
 
     var poll_for_status = function(response) {
-      var feed_url = jQuery('#podcast_feed_url').attr('value');
+      var feed_url = jQuery('#feed_url').attr('value');
       var form_clone = jQuery('#added_podcast').clone();
       form_clone.attr('id', null);
       form_clone.find('.text').attr('value', feed_url);
       form_clone.show();
 
-      jQuery('#podcast_feed_url').val("");
+      jQuery('#feed_url').val("");
       jQuery('#added_podcast_list').append(form_clone);
 
 

1852621e486fd814709acb541a3b15807c60deee
Justin Camerer <iamjwc@gmail.com>
2008-10-15 13:57:25 -0400

    We want to add bitrate to feed, not source

--- db/migrate/20081014220556_add_bitrate_to_source.rb
+++
@@ -1,9 +0,0 @@
-class AddBitrateToSource < ActiveRecord::Migration
-  def self.up
-    add_column :sources, :bitrate, :integer
-  end
-
-  def self.down
-    remove_column :sources, :bitrate
-  end
-end

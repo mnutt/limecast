0a44809bc78f67b52a9a8bec294a0a26e7cee987
Justin Camerer <iamjwc@gmail.com>
2008-10-27 17:33:12 -0400

    Spacing

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -4,7 +4,7 @@ describe Tagging, "being created" do
   it 'should map a bad tag to good tag' do
     good_tag = Tag.create :name => "good"
     bad_tag  = Tag.create :name => "bad", :map_to => good_tag
-    
+
     tagging = Tagging.create :tag => bad_tag
 
     tagging.tag.should == good_tag

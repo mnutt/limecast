e29a59ecf1fca5715b353296e0d33f96c7a0a17b
Justin Camerer <iamjwc@gmail.com>
2008-10-27 14:55:21 -0400

    Added mapping from one tag to another

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -3,4 +3,14 @@ class Tagging < ActiveRecord::Base
   belongs_to :tag
 
   named_scope :podcasts, :conditions => {:taggable_type => 'podcast'}
+
+  before_save :map_to_different_tag
+
+  protected
+
+  def map_to_different_tag
+    return if self.tag.nil?
+
+    self.tag = self.tag.map_to until self.tag.map_to.nil?
+  end
 end

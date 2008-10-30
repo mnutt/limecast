0f6ac03ca996cca2e35b7f9729159613184eaa6d
Justin Camerer <iamjwc@gmail.com>
2008-10-30 15:55:13 -0400

    Changed query param name to 'q'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @users    = User.search(params[:query])
-    @podcasts = Podcast.search(params[:query])
-    @episodes = Episode.search(params[:query])
-    @comments = Comment.search(params[:query])
+    @users    = User.search(params[:q])
+    @podcasts = Podcast.search(params[:q])
+    @episodes = Episode.search(params[:q])
+    @comments = Comment.search(params[:q])
   end
 end

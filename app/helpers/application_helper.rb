fd3377f94eabadc514d9102f3a9a4a79f669bae8
Justin Camerer <iamjwc@gmail.com>
2008-10-31 17:14:30 -0400

    Replaced link_to_thing with link_to_podcast

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,8 +16,8 @@ module ApplicationHelper
     link_to "<span class='searched'>#{link_text}</span>", user_url(user), :class => 'icon user'
   end
 
-  def link_to_thing(thing)
-    link_to "#{image_tag(thing.logo.url(:icon))} <span class='searched'>#{h(thing.custom_title)}</span>", polymorphic_url(thing), :class => 'inline_icon'
+  def link_to_podcast(podcast)
+    link_to "#{image_tag(podcast.logo.url(:icon))} <span class='searched'>#{h(podcast.custom_title)}</span>", podcast_url(podcast), :class => 'inline_icon'
   end
 
   def relative_time(date, abbr=true)

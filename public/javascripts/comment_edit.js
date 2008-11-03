99285611538fa72789743b3a8b0cd6f89efb8d7e
Justin Camerer <iamjwc@gmail.com>
2008-11-03 16:42:31 -0500

    Cleaning up views

--- public/javascripts/comment_edit.js
+++ public/javascripts/comment_edit.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function(){
   jQuery('li.review').map(function(){
-    var show_div = jQuery(this).find('div.show_review');
-    var edit_div = jQuery(this).find('div.edit_review');
+    var show_div  = jQuery(this).find('div.show');
+    var edit_form = jQuery(this).find('form.edit');
 
     show_div.find('a.edit').click(function(){
       show_div.hide();
-      edit_div.show();
+      edit_form.show();
       return false;
     });
 
-    edit_div.find('form').submit(function(){
+    edit_form.submit(function(){
       show_div.show();
-      edit_div.hide();
+      edit_form.hide();
 
       jQuery.ajax({
         type: 'post',
@@ -23,7 +23,7 @@ jQuery(document).ready(function(){
       var form_comment_body  = jQuery(this).find('#comment_body').val();
 
       show_div.find('h4').text(form_comment_title);
-      show_div.find('span.comment_body').text(form_comment_body);
+      show_div.find('.body').text(form_comment_body);
 
       return false;
     });

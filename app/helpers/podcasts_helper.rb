56e623782685b42f253c10402ba9ad0c8d9c9e1a
Tieg Zaharia <tieg.zaharia@gmail.com>
2008-11-10 14:49:53 -0500

    Making podcast#show validate

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -1,5 +1,5 @@
 module PodcastsHelper
   def rss_link(feed)
-    "<link rel='alternate' type='application/rss+xml' title='#{feed.podcast.title}' href='#{feed.url}' />"
+    '<link rel="alternate" type="application/rss+xml" title="#{feed.podcast.title}" href="#{feed.url}" />'
   end
 end

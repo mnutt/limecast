a742c6c49f2d82f929ec46dc179ccb557c57766b
Michael Nutt <michael@nuttnet.net>
2008-11-14 11:42:25 -0500

    Add specs for Add Podcast

--- spec/spec_ui_helper.rb
+++ spec/spec_ui_helper.rb
@@ -3,14 +3,20 @@ require 'safariwatir'
 require 'hpricot'
 require File.dirname(__FILE__) + '/spec_helper'
 
+Spec::Runner.configure do |config|
+  config.before(:all) { setup_browser }
+end
+
 def setup_browser
+  return if @browser
   @browser = Watir::Safari.new
   @browser.extend(BrowserExtensions)
   @browser.url = "http://localhost:3003"
 end
 
 def teardown_browser
-  browser.close rescue nil
+  return if @browser.nil?
+  @browser.close rescue nil
 end
 
 def browser
@@ -38,5 +44,21 @@ module BrowserExtensions
 
   def go(url)
     self.goto([@url, url].join)
+  end
+end
+
+module Watir
+  module Container
+    class GenericElement < ContentElement
+      attr_reader :tag
+      def initialize(tag, scripter, how, what)
+        @tag = tag
+        super(scripter, how, what)
+      end
+    end
+
+    def element(tag, how, what)
+      GenericElement.new(tag, scripter, how, what)
+    end
   end
 end

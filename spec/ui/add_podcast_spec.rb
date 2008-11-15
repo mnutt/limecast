81e5e2d299d528858c39579dff2abdf7d073e0e1
Michael Nutt <michael@nuttnet.net>
2008-11-14 19:21:33 -0500

    Extracted out signin method to spec_ui_helper

--- spec/ui/add_podcast_spec.rb
+++ spec/ui/add_podcast_spec.rb
@@ -2,8 +2,6 @@ require File.dirname(__FILE__) + '/../spec_ui_helper'
 
 describe "Adding podcast while logged out" do
   before(:each) do
-    sleep(1)
-    Feed.destroy_all
     browser.go("/add")
     browser.text_field(:name, "feed[url]").set("http://feeds.feedburner.com/WinelibraryTV")
     browser.button(:value, "Add").click
@@ -22,18 +20,10 @@ end
 
 describe "Adding podcast while logged in" do
   before(:each) do
-    @user = Factory.create(:user)
+    sign_in
     browser.go("/add")
-    browser.element("LI", :class, "signup").click
-    browser.text_field(:name, "user[login]").set(@user.login)
-    browser.text_field(:name, "user[password]").set(@user.password)
-    browser.button(:value, "Sign in").click
-    sleep(3)
-    browser.button(:value, "Sign in").click
-    browser.button(:value, "Sign in").click
-    sleep(3)
-    #browser.text_field(:name, "feed[url]").set("http://feeds.feedburner.com/WinelibraryTV")
-    #browser.button(:value, "Add").click
+    browser.text_field(:name, "feed[url]").set("http://feeds.feedburner.com/WinelibraryTV")
+    browser.button(:value, "Add").click
   end
 
   it 'should not show the inline signin' do

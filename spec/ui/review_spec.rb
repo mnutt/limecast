8252be7a200f38892912635d6f3525129ebca80d
Justin Camerer <iamjwc@gmail.com>
2008-11-17 11:54:06 -0500

    This test should work

--- spec/ui/review_spec.rb
+++ spec/ui/review_spec.rb
@@ -7,11 +7,19 @@ describe "Adding review to a podcast" do
     @feed.refresh
     @podcast = @feed.podcast
     browser.go(@podcast.clean_url)
+
+    @user = Factory.create(:user)
   end
 
   it 'should be prompt user with a login box if they post a review without being logged in' do
     browser.text_field(:name, "review[title]").set("Diggnation Rulez")
     browser.text_area(:name, "review[body]").set("I think diggnation is the coooooolest show...")
     browser.button(:value, "Save").click
+
+    browser.text_field(:id, "login_after_adding_review").set(@user.login)
+    browser.text_field(:id, "password_after_adding_review").set(@user.password)
+    browser.button(:id, "signin_after_adding_review").click
+
+    html.should_not have_tag("div.after_adding_review div.response_container", /Email/)
   end
 end

187aaf5cbaf7737393d65670ccafebeadc67276a
Justin Camerer <iamjwc@gmail.com>
2008-11-20 15:01:08 -0500

    Separated review rating javascript from podcast new javascript

--- public/javascripts/podcast_edit.js
+++ public/javascripts/podcast_edit.js
@@ -33,24 +33,8 @@ $(document).ready(function(){
     }
   });
 
-  $('li.review').map(function(){
-    var review = $(this);
-
-    review.find('a.rate').map(function(){
-      $(this).mustBeLoggedInBeforeSubmit({
-        quick_signin: review.find('.quick_signin'),
-        success: function(resp) {
-          window.location.reload();
-        }
-      });
-    });
-  });
-
   $('.quick_signin.inline').quickSignIn({
     success: function(){ window.location.reload(); }
   });
-
-
-
 });
 

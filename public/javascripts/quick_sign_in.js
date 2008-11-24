15568cbd7284dd21129262c2ff0963d1d1f894dc
Tieg Zaharia <tieg.zaharia@gmail.com>
2008-11-24 15:09:56 -0500

    I entered my correct user and pass, and clicked Sign up. The box expanded, when it should have logged me in

--- public/javascripts/quick_sign_in.js
+++ public/javascripts/quick_sign_in.js
@@ -8,22 +8,34 @@ $.fn.extend({
       // We only want to submit the form if the sign in button is no longer there.
       var should_submit = me.find('input.signin_button').css('display') == 'none';
 
-      me.find('.sign_up').show();
-      me.find('.sign_up input').focus();
-      me.find('input.signin_button').hide();
-
       if(!opts.ajax) {
-	// Set password message and focus password field if it is left blank
-	if(me.find('input.password').val() == "") {
-	  me.find('div.response_container').text("Please choose a password");
-	  me.find('input.password').focus();
-	}
-
-	// Set username message and focus username field if it is left blank
-	if(me.find('input.login').val() == "") {
-	  me.find('div.response_container').text("Choose your new user name");
-	  me.find('input.login').focus();
-	}
+        // Set password message and focus password field if it is left blank
+        if(me.find('input.password').val() == "") {
+          me.find('div.response_container').text("Please choose a password");
+          me.find('input.password').focus();
+        }
+        
+        // Set username message and focus username field if it is left blank
+        if(me.find('input.login').val() == "") {
+          me.find('div.response_container').text("Choose your new user name");
+          me.find('input.login').focus();
+        }
+      } else {
+        $.ajax({
+          type:    'post',
+          url:     me.find('form').attr('action'),
+          data:    me.find('form').serialize(),
+          dataType: "json",
+          success: function(resp){
+            if(resp.success) {
+              window.location.reload();
+            } else {
+              me.find('.sign_up').show();
+              me.find('.sign_up input').focus();
+              me.find('input.signin_button').hide();
+            }
+          }
+        });
       }
 
       // Set the forms action to /users to call UsersController#create
@@ -49,14 +61,13 @@ $.fn.extend({
                 me.find('.response_container').html(resp.html);
                 me.find('.response_container').fadeIn();
               } else me.find('.response_container').html(resp.html);
-               
             }
 
             // Call handlers
             if(resp.success && opts.success) { opts.success(resp); }
             if(!resp.success && opts.error)  { opts.error(resp); }
           }
-	});
+        });
 
         return false;
       });

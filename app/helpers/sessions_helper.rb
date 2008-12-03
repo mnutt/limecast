d95c44e0634736bec0b2b6454d79b8407e8901cf
Tieg Zaharia <tieg.zaharia@gmail.com>
2008-12-03 11:39:31 -0500

    escaped javascript in JSON response to fix bug

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,19 @@
 module SessionsHelper
   def create_session_error(params)
+    forgot_password = link_to("I forgot my password", forgot_password_url)
+    signup          = link_to('Sign Up', '#', :class => 'inline_signup_button')
+
     if params[:user][:login].blank?
       return 'Please type your username'
     elsif @unknown_email
-      return 'This email is new to us.  Are you trying to <a href="#" class="inline_signup_button">Sign Up</a>?'
+      return 'This email is new to us. Are you trying to #{signup}?'
     elsif @unknown_user
-      return 'This user is new to us. Are you trying to <a href="#" class="inline_signup_button">Sign Up</a>?'
+      return "This user is new to us. Are you trying to #{signup}?"
     elsif params[:user][:password].blank?
-      return 'Please type your password'
+      return "Please type your password"
     else
-      return 'User and password don\'t match. <%= link_to %{I forgot my password}, forgot_password_url -%>'
+      return "User and password don't match. #{forgot_password}"
     end
   end
 end
-
-
 

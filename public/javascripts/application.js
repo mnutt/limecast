530fc7a585b4ddbd658bb87cca29a8a49352e0df
Justin Camerer <iamjwc@gmail.com>
2008-12-08 12:03:33 -0500

    Fixed hover and focus behavior method

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,10 +6,10 @@ if(typeof $=='undefined') throw("application.js requires the $ JavaScript framew
 $.fn.extend({
   hoverAndFocusBehavior: function() {
     var me = $(this);
-    me.mouseover(function() { me.addClass('hover');    });
-    me.mouseout(function()  { me.removeClass('hover'); });
-    me.focus(function() { me.addClass('focus').removeClass('hover');    });
-    me.blur(function()  { me.removeClass('focus').removeClass('hover'); });
+    me.mouseover(function() { $(this).addClass('hover');    });
+    me.mouseout(function()  { $(this).removeClass('hover'); });
+    me.focus(function()     { $(this).addClass('focus').removeClass('hover');    });
+    me.blur(function()      { $(this).removeClass('focus').removeClass('hover'); });
   }
 });
 $(document).ready(function() {

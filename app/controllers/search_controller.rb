fb5364d422e0a92a974772a1a6ed6e1d46658a6b
Tieg Zaharia <tieg.zaharia@gmail.com>
2008-12-15 15:30:24 -0500

    clearing nils from SearchController results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @users    = User.search(params[:q])
-    @podcasts = Podcast.scoped(:conditions => {:id => Podcast.search(params[:q]).map(&:id)}).sorted
-    @episodes = Episode.search(params[:q])
-    @reviews  = Review.search(params[:q])
+    @users    = User.search(params[:q]).compact
+    puts "@users is #{@users.inspect}"
+    @podcasts = Podcast.scoped(:conditions => {:id => Podcast.search(params[:q]).compact.map(&:id)}).sorted
+    @episodes = Episode.search(params[:q]).compact
+    @reviews  = Review.search(params[:q]).compact
   end
 end

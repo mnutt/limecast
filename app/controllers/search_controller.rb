89d3352559ce214b5a3c60463d9ace6351fd0b17
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-01-14 18:33:09 -0500

    adding a before_filter to check for q in search actions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,42 +1,42 @@
 class SearchController < ApplicationController
+  before_filter :check_for_query
+
   def index
-    if params[:q]
-      params[:q] += " podcast:#{params[:podcast]}" if params[:podcast]
-      @parsed_q = (@q = params[:q].strip).dup
+    params[:q] += " podcast:#{params[:podcast]}" if params[:podcast]
+    @parsed_q = (@q = params[:q].strip).dup
 
-      extract_podcast!
-      extract_tags!
-      extract_only!
+    extract_podcast!
+    extract_tags!
+    extract_only!
 
-      @parsed_q.strip!
+    @parsed_q.strip!
 
-      raise ActiveRecord::RecordNotFound if instance_variable_defined?(:@podcast) && @podcast.nil?
+    raise ActiveRecord::RecordNotFound if instance_variable_defined?(:@podcast) && @podcast.nil?
 
-      # get all possible results from User, Tag, Feed, Episode, Review, and Podcast.
-      @user     = User.find_by_login(@parsed_q) unless @only && @only != :user
-      @tag      = Tag.find_by_name(@parsed_q) unless @only && @only != :tag
-      @feeds    = (@podcast ? @podcast.feeds : Feed).search(@parsed_q).compact.uniq unless @only && @only != :feeds
-      @episodes = (@podcast ? @podcast.episodes : Episode).search(@parsed_q).compact.uniq unless @only && @only != :episodes
-      @reviews  = (@podcast ? Review.for_podcast(@podcast) : Review).search(@parsed_q).compact.uniq  unless @only && @only != :reviews
-      @podcasts = @podcast ? [@podcast] : Podcast.search(@parsed_q).compact.uniq unless @only && @only != :podcasts
+    # get all possible results from User, Tag, Feed, Episode, Review, and Podcast.
+    @user     = User.find_by_login(@parsed_q) unless @only && @only != :user
+    @tag      = Tag.find_by_name(@parsed_q) unless @only && @only != :tag
+    @feeds    = (@podcast ? @podcast.feeds : Feed).search(@parsed_q).compact.uniq unless @only && @only != :feeds
+    @episodes = (@podcast ? @podcast.episodes : Episode).search(@parsed_q).compact.uniq unless @only && @only != :episodes
+    @reviews  = (@podcast ? Review.for_podcast(@podcast) : Review).search(@parsed_q).compact.uniq  unless @only && @only != :reviews
+    @podcasts = @podcast ? [@podcast] : Podcast.search(@parsed_q).compact.uniq unless @only && @only != :podcasts
 
-      @podcast_groups = Hash.new { |h, k| h[k] = [] } # hash where the keys are the unique podcast ids,
-                                                      # and the values are arrays of their search results
-      def @podcast_groups.add(obj, podcast_id); self[podcast_id] << obj; end
-      def @podcast_groups.count(klass); inject(0) { |count, p| count + p[1].select { |o| o.is_a?(klass) }.size }; end
+    @podcast_groups = Hash.new { |h, k| h[k] = [] } # hash where the keys are the unique podcast ids,
+                                                    # and the values are arrays of their search results
+    def @podcast_groups.add(obj, podcast_id); self[podcast_id] << obj; end
+    def @podcast_groups.count(klass); inject(0) { |count, p| count + p[1].select { |o| o.is_a?(klass) }.size }; end
 
-      # Group all the podcast-related search results by podcast-id
-      @feeds.each    { |f| @podcast_groups.add(f, f.podcast.id) } if @feeds
-      @episodes.each { |e| @podcast_groups.add(e, e.podcast.id) } if @episodes
-      @reviews.each  { |r| @podcast_groups.add(r, r.episode.podcast.id) } if @reviews
-      @podcasts.each { |p| @podcast_groups.add(p, p.id) } if @podcasts
+    # Group all the podcast-related search results by podcast-id
+    @feeds.each    { |f| @podcast_groups.add(f, f.podcast.id) } if @feeds
+    @episodes.each { |e| @podcast_groups.add(e, e.podcast.id) } if @episodes
+    @reviews.each  { |r| @podcast_groups.add(r, r.episode.podcast.id) } if @reviews
+    @podcasts.each { |p| @podcast_groups.add(p, p.id) } if @podcasts
 
-      # rewrites @podcasts to have all the podcasts we need
-      # FIXME Added pagination, but now we're merely skipping all the results from the other podcasts we
-      #       searched; might get out of hand later with more podcasts.
-      @podcasts = (@tags ? Podcast.tagged_with(@tags) : Podcast).
-        paginate(:conditions => {:id => @podcast_groups.keys}, :page => (params[:page]||1), :per_page => 3).compact #.sorted
-    end
+    # rewrites @podcasts to have all the podcasts we need
+    # FIXME Added pagination, but now we're merely skipping all the results from the other podcasts we
+    #       searched; might get out of hand later with more podcasts.
+    @podcasts = (@tags ? Podcast.tagged_with(@tags) : Podcast).
+      paginate(:conditions => {:id => @podcast_groups.keys}, :page => (params[:page]||1), :per_page => 3).compact #.sorted
   end
 
   def google
@@ -44,6 +44,10 @@ class SearchController < ApplicationController
   end
 
   protected
+    def check_for_query
+      redirect_to params.merge(:q => '') unless params[:q]
+    end
+
     # match Podcast, ex: "games podcast:Diggnation"
     def extract_podcast!
       @parsed_q.gsub!(/(\b)*podcast\:(\S*)(\b)*/i, "")
@@ -61,4 +65,5 @@ class SearchController < ApplicationController
       @parsed_q.gsub!(/(\b)*only\:(\S*)(\b)*/i, "")
       @only = [:feeds, :episodes, :reviews, :podcasts, :tag, :user].detect{|o| o == $2.to_sym} unless $2.blank?
     end
+
 end

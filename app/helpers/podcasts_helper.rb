176f21310f67fcf062df78e62f28edaaca84f715
Justin Camerer <iamjwc@gmail.com>
2009-02-17 18:21:32 -0500

    Fixed some image problems

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -2,4 +2,12 @@ module PodcastsHelper
   def rss_link(feed)
     %{<link rel="alternate" type="application/rss+xml" title="#{feed.formatted_bitrate} #{feed.apparent_format}" href="#{feed.url}" />}
   end
+
+  def cover_art(podcast)
+    if podcast.logo?
+      podcast.logo.url(:small)
+    else
+      "/imgs/no_cover.png"
+    end
+  end
 end

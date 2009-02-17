bc2163474bf27f5d29e3160fffe7065a27499ce9
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-02-17 12:24:32 -0500

    fixed the podcast_slug problem for episoes and reviews on working_master

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,7 +2,7 @@ class EpisodesController < ApplicationController
   before_filter :login_required, :only => [:favorite]
 
   def index
-    @podcast  = Podcast.find_by_clean_url(params[:podcast])
+    @podcast  = Podcast.find_by_clean_url(params[:podcast_slug])
     raise ActiveRecord::RecordNotFound if @podcast.nil?
     @episodes = @podcast.episodes.find(:all, :include => [:podcast], :order => "published_at DESC")
     @newest_episode = @episodes.first
@@ -12,15 +12,15 @@ class EpisodesController < ApplicationController
 
   def search
     @q        = params[:q]
-    @podcast  = Podcast.find_by_clean_url(params[:podcast])
+    @podcast  = Podcast.find_by_clean_url(params[:podcast_slug])
     @episodes = @podcast.episodes.search(@q, :include => [:podcast]).compact.uniq.sort_by(&:published_at)
     @feeds    = @podcast.feeds
     render :action => 'index'
   end
 
   def show
-    @podcast = Podcast.find_by_clean_url(params[:podcast])
-    raise ActiveRecord::RecordNotFound if @podcast.nil? || params[:podcast].nil?
+    @podcast = Podcast.find_by_clean_url(params[:podcast_slug])
+    raise ActiveRecord::RecordNotFound if @podcast.nil? || params[:podcast_slug].nil?
 
     @episode = @podcast.episodes.find_by_clean_url(params[:episode])
     raise ActiveRecord::RecordNotFound if @episode.nil? || params[:episode].nil?
@@ -33,7 +33,7 @@ class EpisodesController < ApplicationController
   end
 
   def info
-    @podcast = Podcast.find_by_clean_url(params[:podcast])
+    @podcast = Podcast.find_by_clean_url(params[:podcast_slug])
     @episode = @podcast.episodes.find_by_clean_url(params[:episode])
 
     render :layout => "info"

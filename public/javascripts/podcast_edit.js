e101d9aab2a6d758b84e099c87134ae795b0d29d
Justin Camerer <iamjwc@gmail.com>
2009-02-18 13:29:42 -0500

    Fixed add to favorites display bug

--- public/javascripts/podcast_edit.js
+++ public/javascripts/podcast_edit.js
@@ -1,21 +1,4 @@
 attachPodcastEditEvents = function(){
-  // This is the favorite_link on Podcast#show pages
-  $('a.favorite_link').click(function() {
-    favorite_link = $(this);
-    favorite_url = favorite_link.attr('href');
- 
-    $.post(favorite_url, {}, function(resp) {
-      if(resp.logged_in) {
-        favorite_link.replaceWith('<span><img src="/images/icons/favorite.png" class="inline_icon" alt="" />My Favorite</span>');
-      } else {
-        return $.quickSignIn.attach($('.quick_signin_container.after_favoriting'), 
-          {message:'Sign up or sign in to save your favorite.'});
-      }
-    }, 'json');
-    
-    return false;
-  });
-  
   $('a.make_primary').click(function(link){
     $.ajax({
        type: 'post',

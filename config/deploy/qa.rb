24b8080347ffd92b223ff811de25762f08561581
Michael Nutt <michael@nuttnet.net>
2009-02-23 16:37:40 -0500

    QA deploys via copy

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -4,6 +4,9 @@ set :user, "limecast"
 set :remote_home, "/var/www"
 set :deploy_to, "/var/www/limecast"
 
+set :deploy_via,      :copy
+set :deploy_strategy, :export
+
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true

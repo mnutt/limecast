882e6ceca7db3cca133f8136404bd04cfd2fc2d6
Michael Nutt <michael@nuttnet.net>
2009-02-23 10:50:25 -0500

    Deploy for QA

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,5 +1,8 @@
 set :branch, "working_master"
 set :domain, "localhost"
+set :user, "root"
+set :remote_home, "/var/www/html"
+set :deploy_to, "/var/www/html/limecast.com"
 
 role :web, domain
 role :app, domain

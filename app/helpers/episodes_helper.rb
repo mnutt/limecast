dd156949e0c1cb0acd48a95c235c87d2b46aa406
Justin Camerer <iamjwc@gmail.com>
2009-02-24 12:18:44 -0500

    Geometry was wrong. :-|

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -2,7 +2,7 @@ module EpisodesHelper
   def scaled_resolution(res)
     scale_to_width = 460
 
-    height = ((res[:width].to_f / scale_to_width) * res[:height]) rescue nil
+    height = ((scale_to_width / res[:width].to_f) * res[:height]) rescue nil
 
     { :height => height, :width => scale_to_width }
   end

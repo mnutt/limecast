9e9374b36b5686004b7bad4f4e2db9371f0f3b38
Justin Camerer <iamjwc@gmail.com>
2009-02-24 12:02:38 -0500

    Scale videos appropriately if possible

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,2 +1,9 @@
 module EpisodesHelper
+  def scaled_resolution(res)
+    scale_to_width = 460
+
+    height = ((res[:width].to_f / scale_to_width) * res[:height]) rescue nil
+
+    { :height => height, :width => scale_to_width }
+  end
 end

96795a3f71bedd6c7814a013b649bf70a3f80e8d
Justin Camerer <iamjwc@gmail.com>
2009-03-05 12:23:34 -0500

    Added iTunes link helper

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -23,6 +23,11 @@ module FeedsHelper
     [feed.apparent_resolution, "bitrate: #{feed.formatted_bitrate}"].compact.join(" | ")
   end
 
+  def link_to_itunes(feed)
+    url = "http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=#{feed.itunes_link}"
+    link_to("iTunes", url, :class => "itunes", :title => "View #{feed.podcast.title} in iTunes")
+  end
+
   def link_to_feed_size(feed, type, &url)
     link_to default_bitrate_label(feed.bitrate),
       url.call(feed),

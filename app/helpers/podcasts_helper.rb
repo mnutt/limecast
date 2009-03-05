6663ad698872fdfa6bd586fcbe345f2938bb22bb
Justin Camerer <iamjwc@gmail.com>
2009-03-05 12:36:14 -0500

    Moved tags into partial

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -3,6 +3,10 @@ module PodcastsHelper
     %{<link rel="alternate" type="application/rss+xml" title="#{feed.formatted_bitrate} #{feed.apparent_format}" href="#{feed.url}" />}
   end
 
+  def link_to_podcast_home(podcast)
+    link_to h(podcast.clean_site), h(podcast.site)
+  end
+
   def cover_art(podcast, size = :small)
     if podcast.logo?
       podcast.logo.url(size)

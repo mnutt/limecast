c6d52526c3ce8e3da2cad8f4dea83ec04a5f06b4
Justin Camerer <iamjwc@gmail.com>
2009-03-05 14:36:37 -0500

    Moved googleads and willpaginate into helpes

--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -3,6 +3,14 @@ module PodcastsHelper
     %{<link rel="alternate" type="application/rss+xml" title="#{feed.formatted_bitrate} #{feed.apparent_format}" href="#{feed.url}" />}
   end
 
+  def paginate_podcasts(podcasts)
+    will_paginate podcasts,
+      :previous_label => '<img src="../imgs/icons/left-arrow.gif" title="Previous page" />',
+      :next_label     => '<img src="../imgs/icons/right-arrow.gif" title="Next page" />',
+      :inner_window   => 1,
+      :outer_window   => 1
+  end
+
   def link_to_podcast_home(podcast)
     link_to h(podcast.clean_site), h(podcast.site)
   end

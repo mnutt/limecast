7f2acfa7645ad0c268ed17d37d65273697f5cfe7
Justin Camerer <iamjwc@gmail.com>
2009-03-06 17:29:08 -0500

    Mad episode refactoring

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -10,4 +10,12 @@ module EpisodesHelper
   def link_to_episode(ep)
     link_to ep.title, episode_url(:podcast_slug => ep.podcast, :episode => ep)
   end
+
+  def display_episode_screenshot(ep)
+    if ep.sources.first && ep.sources.first.screenshot?
+      image_tag(ep.sources.first.screenshot(:square).to_s, :height => 55, :width => 55)
+    else
+      image_tag(cover_art(ep.podcast, :square), :height => 55, :width => 55)
+    end
+  end
 end

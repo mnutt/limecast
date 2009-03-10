67b2fb4e7111bc3824959adc92fd200553e9c00f
Justin Camerer <iamjwc@gmail.com>
2009-03-10 14:42:32 -0400

    Removed search method from tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,18 +11,6 @@ class TagsController < ApplicationController
     @podcasts = @tag.podcasts.paginate(:page => (params[:page] || 1), :per_page => params[:limit] || 10)
   end
 
-  def search
-    @q = params[:q]
-    @tag = Tag.find_by_name(params[:tag])
-    raise ActiveRecord::RecordNotFound if @tag.nil?
-
-    # FIXME This is a hack; you should be able to do Podcast.search(@q, :with => {:tagged_ids => @tag.id}).compact,
-    #       but that's pulling up podcasts outside of the tag right now... (look at Reviews#search for
-    #       a working example :O
-    @podcasts = Podcast.search(@q, :include => [:tags]).compact.select { |p| p.tags.include?(@tag) }
-    render :action => 'show'
-  end
-
   def info
     @tag = Tag.find_by_name(params[:tag]) or raise ActiveRecord::RecordNotFound
     render :layout => 'info'

b46580642d73091222b18ee5c6e0ad2733749b26
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-03-23 18:07:25 -0400

    refactoring application.js to the max

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,111 +19,74 @@ $.ajaxSetup({
 
 // Method hooks up all of the input text boxes that should have default labels
 function defaultText() {
-  var ids = [ '#feed_url', '#accounts_forgot_password #email', '#search_podcast_q',
-               '.limecast_form .new_podcast_feed_url', '#q', '#user_tagging_tag_string'];
-
-  $(ids.join(',')).inputDefaultText();
+  $("#feed_url, #accounts_forgot_password #email, #search_podcast_q, " + 
+    ".limecast_form .new_podcast_feed_url, #q, #user_tagging_tag_string").inputDefaultText();
 }
 
-$(document).ready(function() {
-  defaultText();
-
-  // Handles truncated "more" and "less" links
+// Handles truncated "more" and "less" links
+function truncatedText() {
   $("a.truncated").click(function(){
     var text = $(this).text();
     var oppositeText = text == "less" ? "more" : "less";
 
-    $(this)
-      .text(oppositeText)
-      .parent()
-        .find(".truncated." + oppositeText)
-          .hide()
-        .end()
-        .find(".truncated." + text)
-          .show()
-        .end();
+    $(this).text(oppositeText);
+    $(this).parent().find(".truncated." + oppositeText).hide();
+    $(this).parent().find(".truncated." + text).show();
 
     return false;
-  });
+  }); 
+}
 
-
-  if($('.podcast.show').size() && !$('.podcast.new.show').size()) {
-    $('#podcast_episodes').dataTable({
-      "aaSorting": [[ 3, "desc" ]],
-      "aoColumns": [ 
-           /* Title */   { "bSortable": false },
-           /* Description */  { "bVisible":    false, "bSortable": false },
-           /* Runtime */ { "bSortable": false },
-           /* Date released */  null
-           ],
-        "bStateSave": true,
-        "bProcessing": true
-      });
-  }
+function podcastEpisodeSorting() {
+  var title = { "bSortable": false };
+  var description = { "bVisible":    false, "bSortable": false };
+  var runtime = { "bSortable": false };
+  var date_released = null;
   
-  // Favorite link
+  $('#podcast_episodes').dataTable({
+    "aaSorting": [[ 3, "desc" ]],
+    "aoColumns": [ title, description, runtime, date_released ],
+    "bStateSave": true,
+    "bProcessing": true
+  });
+}
+
+function favoriteLink() {
   $('a.favorite_link').click(function() {
     if(LOGGED_IN) {
       var favorite_link = $(this);
       var favorite_url = favorite_link.attr('href');
   
-      $.post(favorite_url, {}, function(resp) {
-        if(resp.logged_in) window.location = window.location;
-      }, 'json');
+      $.post(favorite_url, {}, function(resp) { window.location.reload(); }, 'json');
     }
     return false;
   });
-  
-  // Cluetips 
+}
+
+function setupNotLoggedInCluetips() {
   if(!LOGGED_IN) {
-    $('#podcast_tag_form .submit').cluetip({
-      local: true, 
-      hideLocal: true, 
-      arrows: true, 
-      width: 350,  
-      sticky: true,
-      showTitle: false, 
-      activation: 'click', 
-      positionBy: 'auto', 
-      leftOffset: -50,
-      onShow: function(){
-        $.quickSignIn.setup();
-      }
-    });
+    var default_options = { local: true, arrows: true, width: 350, showTitle: false };
+    $('a.tips').cluetip(default_options);
 
-    $('a.tips').cluetip({local: true, hideLocal: true, arrows: true, width: 350,  showTitle: false});
+    default_options = $.extend({activation: 'click', sticky: true, onShow: function(){$.quickSignIn.setup()}}, default_options);
 
-    $('a.login').cluetip({
-      local: true, 
-      hideLocal: true, 
-      arrows: true, 
-      width: 350,  
-      sticky: true,
-      showTitle: false, 
-      activation: 'click', 
-      positionBy: 'bottomTop', 
-      topOffset: 25,
-      onShow: function(){
-        $.quickSignIn.setup();
-      }
-    });
+    var options = $.extend({positionBy: 'auto', leftOffset: -50}, default_options);
+    $('#podcast_tag_form .submit').cluetip(options);
+    
+    default_options = $.extend({positionBy: 'bottomTop', topOffset: 25}, default_options);
+    $('a.login').cluetip(default_options); 
 
-    $('a.signup').cluetip({
-      local: true, 
-      hideLocal: true, 
-      arrows: true, 
-      width: 350,  
-      sticky: true,
-      showTitle: false, 
-      activation: 'click', 
-      positionBy: 'bottomTop', 
-      topOffset: 25,
-      onShow: function(){
-        $.quickSignIn.showSignUp();
-        $.quickSignIn.setup();
-      }
-    });
+    default_options = $.extend({onShow: function(){ $.quickSignIn.showSignUp(); $.quickSignIn.setup(); }}, default_options);
+    $('a.signup').cluetip(default_options);
   }
+}
+
+$(document).ready(function() {
+  defaultText();
+  truncatedText();
+  podcastEpisodeSorting();
+  favoriteLink();
+  setupNotLoggedInCluetips();
 
   
   // Add Podcast link

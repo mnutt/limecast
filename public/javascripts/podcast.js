4d7cf7bcbabacb6fbf9f835e4171cb6176b465af
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-03-24 11:38:50 -0400

    more refactoring of js

--- public/javascripts/podcast.js
+++ public/javascripts/podcast.js
@@ -14,4 +14,17 @@ function podcastEpisodeSorting() {
 
 $(document).ready(function() {
   podcastEpisodeSorting();
+  $("#subscribe_options li a").click(function(){ return false; });
+  $("#s_options_toggle").click(function(e){
+    $("#subscribe_options_container").slideDown("fast");
+
+    // similar to jquery.dropdown.js
+    if($("#overlay").size() == 0) $('body').append("<div id=\"overlay\"></div>");
+    $("#overlay").click(function(){
+      $("#subscribe_options_container").slideUp("fast");
+      $(this).remove();
+    });
+
+    e.preventDefault();
+  });
 }

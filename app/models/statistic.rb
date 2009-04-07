bc6edf09ce97a8268c045c508bec40ce8b5b1141
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-04-07 12:23:16 -0400

    fixing the Statistic named_scope :by_month_and_year

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,3 +1,3 @@
 class Statistic < ActiveRecord::Base
-  named_scope :by_month_and_year, :limit => 1, :group => "DATE_FORMAT(created_at,'%b %y')", :order => :created_at
+  named_scope :by_month_and_year, :group => "DATE_FORMAT(created_at,'%b %y')", :order => :created_at
 end

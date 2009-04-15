e8b377c3fb59671bb222065d195b87ba6a359f51
Justin Camerer <iamjwc@gmail.com>
2009-04-15 14:48:16 -0400

    Oops

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,32 +1,32 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Tagging, "being created" do
-#  before do
-#    @podcast = Factory.create(:podcast)
-#  end
-#
-#  it 'should map a bad tag to good tag' do
-#    good_tag = Tag.create :name => "good"
-#    bad_tag  = Tag.create :name => "bad", :map_to => good_tag
-#
-#    tagging = Tagging.create :tag => bad_tag
-#
-#    tagging.tag.should == good_tag
-#  end
-#
-#  it 'should allow any number of tags per podcast' do
-#    adding_tagging = lambda { @podcast.tags << Factory.create(:tag) }
-#
-#    10.times { adding_tagging.should change(Tagging, :count).by(1) }
-#  end
-#
-#  it 'should not allow duplicate taggings' do
-#    tag = Factory.create(:tag)
-#    adding_tagging = lambda { Tagging.create!(:podcast => @podcast, :tag => tag) }
-#
-#    adding_tagging.should change(Tagging, :count).by(1)
-#    adding_tagging.should raise_error(ActiveRecord::RecordInvalid)
-#  end
+  before do
+    @podcast = Factory.create(:podcast)
+  end
+
+  it 'should map a bad tag to good tag' do
+    good_tag = Tag.create :name => "good"
+    bad_tag  = Tag.create :name => "bad", :map_to => good_tag
+
+    tagging = Tagging.create :tag => bad_tag
+
+    tagging.tag.should == good_tag
+  end
+
+  it 'should allow any number of tags per podcast' do
+    adding_tagging = lambda { @podcast.tags << Factory.create(:tag) }
+
+    10.times { adding_tagging.should change(Tagging, :count).by(1) }
+  end
+
+  it 'should not allow duplicate taggings' do
+    tag = Factory.create(:tag)
+    adding_tagging = lambda { Tagging.create!(:podcast => @podcast, :tag => tag) }
+
+    adding_tagging.should change(Tagging, :count).by(1)
+    adding_tagging.should raise_error(ActiveRecord::RecordInvalid)
+  end
 
   it 'should find episodes older X minutes ago with the named scope helper' do
     t = Tagging.create :tag_id => 10, :podcast_id => 20, :created_at => 2.hours.ago

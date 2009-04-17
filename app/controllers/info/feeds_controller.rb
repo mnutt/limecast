5472991eb1dc45a0f247b3a60beb63310c33df54
Michael Nutt <michael@nuttnet.net>
2009-04-17 16:12:00 -0400

    Show only approved totals for source hasher

--- app/controllers/info/feeds_controller.rb
+++ app/controllers/info/feeds_controller.rb
@@ -15,8 +15,9 @@ class Info::FeedsController < InfoController
     @sources = Source.find(:all, :conditions => ["hashed_at > ?", 3.days.ago],
                            :limit => 40,
                            :order => "hashed_at DESC")
-    @sources_count = Source.count
-    @unhashed_count = Source.stale.count
+    @sources_count = Source.approved.count
+    @unhashed_count = Source.stale.approved.count
+		@unapproved_count = Source.count - Source.approved.count
     @hashed_count = @sources_count - @unhashed_count
     @percentage = (@hashed_count.to_f / @sources_count.to_f * 100).to_i
     @last_day = Source.count(:conditions => ["hashed_at > ?", 1.day.ago])

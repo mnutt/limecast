c0d79d8e5d6fa27c6e94a5a5aa72c7ecaf0365e1
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-06-10 15:15:14 -0400

    updating qa deploy script for git commit numbers

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -4,10 +4,11 @@ set :user, "limecast"
 set :remote_home, "/var/www"
 set :deploy_to, "/var/www/html"
 
-set :deploy_via,      :copy
-set :copy_cache, true
-set :copy_exclude, [".git"]
-set :deploy_strategy, :export
+# Commenting out so that QA can have the git commit number
+# set :deploy_via,      :copy
+# set :copy_cache, true
+# set :copy_exclude, [".git"]
+# set :deploy_strategy, :export
 
 role :web, domain
 role :app, domain

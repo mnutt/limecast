e678a6fe1aa69963b06a4cfa28a97676da1083a2
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-06-10 16:39:39 -0400

    fixing stage/beta deploy script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,11 @@
 # set your dns to point staging.limecast.com to your staging server. at work it is 10.254.0.240
 set :domain, 'beta.limecast.com'
+set :branch, "master"
+set :user, "limecast"
+set :remote_home, "/var/www"
+set :deploy_to, "/var/www/html"
 
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
+

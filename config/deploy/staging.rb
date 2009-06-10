025d57e6124702b21ec8fe02fee7f068547fc402
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-06-10 15:19:52 -0400

    updating staging to use beta.limecast.com

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 # set your dns to point staging.limecast.com to your staging server. at work it is 10.254.0.240
-set :domain, 'cast-staging'
+set :domain, 'beta.limecast.com'
 
 role :web, domain
 role :app, domain

f54e8139f0d003a93c96ff987081f5a8cd6bae55
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-06-16 14:15:42 -0400

    moving prod to beta.limecast.com and officially removing production stage

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,12 +1,8 @@
-# An alias for production
-set :stage, :production
-
-set :domain, 'limecast.com'
+set :domain, 'beta.limecast.com'
 
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
-
 
 namespace :deploy do
   # Overwriting Cap's symlinked shared/log directory in favor of /var/log/limecast

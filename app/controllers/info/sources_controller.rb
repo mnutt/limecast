ea02aa234a4ae7bc6fb24a2b50897f0f6e6ffe2c
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-06-29 16:30:52 -0400

    removing pagination from Source page (info pages)

--- app/controllers/info/sources_controller.rb
+++ app/controllers/info/sources_controller.rb
@@ -3,11 +3,5 @@ class Info::SourcesController < InfoController
     @podcast = Podcast.find_by_slug(params[:podcast_slug])
     @episode = @podcast.episodes.find_by_slug(params[:episode])
     @source = Source.find(params[:id])
-    if newer_episode = @episode.next_episode
-      @newer = newer_episode.sources.first
-    end
-    if older_episode = @episode.previous_episode
-      @older = older_episode.sources.first
-    end
   end
 end

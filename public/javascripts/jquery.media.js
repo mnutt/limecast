b86c42332fe41fd51e25a282071b46726fa6e09f
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-08-21 16:58:39 -0400

    finished support for video and audio tag, with refinements to JS and volume icon

--- public/javascripts/jquery.media.js
+++ public/javascripts/jquery.media.js
@@ -10,10 +10,12 @@ Number.prototype.secondsToTime = function() {
 jQuery.fn.extend({
   updateMediaProgress: function(){
     var t = $(this);
-    var x = t.attr('currentTime');
-    var y =  (t.attr('duration') || 0);
-    t.parent().find('.time').html(x.secondsToTime() + " / " + y.secondsToTime());
-    t.parent().find('progress div').css('width', (x/y)*100+"%");
+
+    var current = (t.attr('flash_id') ? (soundManager.getSoundById(t.attr('flash_id')).position/1000) : t.attr('currentTime')) || 0;
+    var total = (t.attr('flash_id') ? t.duration() : t.attr('duration')) || 0;
+    if(total == 0) total = 1;
+    t.parent().find('.time').html(current.secondsToTime() + " / " + total.secondsToTime());
+    t.parent().find('.progress div').css('width', (current/total)*100+"%");
   },
 
   // Get the offset position relative to page
@@ -32,40 +34,99 @@ jQuery.fn.extend({
   
   play: function() {
     var t = $(this);
-    t[0].play();
-    if(!t.data("interval") || t.data("interval") == undefined) {
+    if(t.attr('flash_id')) soundManager.play(t.attr('flash_id'));
+    else t[0].play();
+
+    if(!t.data("interval") || t.data("interval") == undefined) { // TODO add a fallback w/whilePlaying for SoundManager sounds
       t.data("interval", window.setInterval(function(){ t.updateMediaProgress(); }, 100));
     };
   },
   
-  pause: function(){
-    t = $(this);
-    t[0].pause();
-    window.clearInterval(t.data("interval"));
+  pause: function() {
+    var t = $(this);
+    if(t.attr('flash_id')) soundManager.pause(t.attr('flash_id'));
+    else t[0].pause();
+
+    window.clearInterval(t.data("interval")); // TODO add a fallback w/whilePlaying for SoundManager sounds
     t.data("interval", null);
+  },
+  
+  paused: function() {
+    var t = $(this);
+    return (t.attr('flash_id') ? soundManager.getSoundById(t.attr('flash_id')).paused : t[0].paused);
+  },
+
+  setCurrentTime: function(pos) {
+    var t = $(this);
+
+    if (t.attr('flash_id')) {
+      soundManager.getSoundById(t.attr('flash_id')).setPosition(pos * 1000);
+    } else { 
+      try { t.attr('currentTime', pos); } catch(e) { console.log("Error: "+e); }; // If it's not fully loaded
+    };
+  },
+  
+  updateTime: function(){ 
+    var t = $(this);
+    t.parent().find('.time').html((0).secondsToTime() + ' / ' + t.duration().secondsToTime());
+  },
+  
+  setVolume: function(vol) {
+    var t = $(this);
+    if (t.attr('flash_id')) soundManager.getSoundById(t.attr('flash_id')).setVolume(vol*100);
+    else t.attr('volume', vol);
+  },
+  
+  duration: function() {
+    var t = $(this);
+    if(t.attr('flash_id')) {
+      var s = soundManager.getSoundById(t.attr('flash_id'));
+      return (s.duration == 0 ? s.durationEstimate : s.duration) / 1000;
+    } else {
+      return t.attr('duration');
+    };
+  },
+  
+  mute: function() {
+    var t = $(this);
+    if(t.attr('flash_id')) soundManager.getSoundById(t.attr('flash_id')).toggleMute();
+    else t.attr('muted', !t.attr('muted'));
+  },
+
+  loadSoundManager: function() {
+    $('<script src="/javascripts/soundmanager2.js" type="text/javascript"></script>').appendTo(document.body);
+    soundManager.url = '/flash/soundmanager2.swf';
+    soundManager.debugMode = false;
   },
 
   fallbackToFlashAudio: function() {
-    this.each(function(){
-      var audio = $(this);
-      var source = $(this).find('source').attr('src');
-      var width = (audio.attr('width') || 540);
-      $('<script src="/flash/audio-player/audio-player.js" type="text/javascript"></script>').appendTo(document.body);
-      AudioPlayer.setup("/flash/audio-player/player.swf", { width: width });
-      AudioPlayer.embed(audio.attr('id'), {  
-          soundFile: source,
-          titles: audio.attr('rel'),
-          autostart: "no"  
+    $.fn.loadSoundManager();
+    var audio_tags = $(this);
+    
+    soundManager.onload = function() {
+      audio_tags.each(function() {
+        var audio = $(this);
+        audio.attr('flash_id', audio.attr('id')+'_flash').attr('is_flash'); // the ref for soundmanager
+        soundManager.createSound({
+          id: audio.attr('flash_id'), 
+          url: audio.find('source').attr('src')
+          // autoload: true,
+          // whileloading: function(){ audio.updateTime(); },
+        });
+        audio.initMediaControls();
       });
-    });
+    };
   },
   
   fallbackToFlashVideo: function() {
-    this.each(function(){
+    var video_tags = $(this);
+
+    video_tags.each(function(){
       var video = $(this);
+      video.attr('is_flash',true); // just so we know it's flash
       var poster = video.attr('poster');
       var source = $(this).find('source').attr('src');
-      var img = $('<img style="visibility: hidden;" src="'+poster+'" />').appendTo(document.body);
+      var img = $('<img style="visibility:; hidden;" src="'+poster+'" />').appendTo(document.body);
       var i = 0;
       var addFlash = function(){
         if(img.attr('width') == 0) {
@@ -78,41 +139,40 @@ jQuery.fn.extend({
             attr('width', width).
             attr('height', height).
             attr('src', '/flash/CastPlayer.swf').
-            appendTo(video);
+            appendTo(video.parent());
         }
       }();
+      video.initMediaControls();
     });
   },
   
   initAudio: function() {
-    var audio = $(this);
-    if(audio.size() == 0) return false;
+    var audio_tags = $(this);
+    if(audio_tags.size() == 0) return false;
 
     // Check for audio tag and audio codec support
-    var format = audio.find('source')[0].src.split('.').pop();
-    if (!audio[0].canPlayType || (audio[0].canPlayType('audio/'+format) != "maybe" && audio[0].canPlayType('audio/'+format) != "probably")) {
-      this.fallbackToFlashAudio();
-      return false;
-    }
-    
-    this.initMediaControls();
+    var format = audio_tags.find('source').attr('src').split('.').pop();
+    if(format == 'mp3' || format == 'mp4') format = 'mpeg';
+    if (!audio_tags[0].canPlayType || (audio_tags[0].canPlayType('audio/'+format) != "maybe" && audio_tags[0].canPlayType('audio/'+format) != "probably"))
+      audio_tags.fallbackToFlashAudio();
+    else
+      $.each(audio_tags, function(i, a){ $(a).show().initMediaControls() });
   },
   
   initVideo: function() {
-    var video = $(this);
-    if(video.size() == 0) return false;
+    var video_tags = $(this);
+    if(video_tags.size() == 0) return false;
 
     // Check for video tag and video codec support
-    if (!video[0].canPlayType || (video[0].canPlayType('video/mp4') == "no" && video[0].canPlayType('video/ogg') == "no")) {
-      this.fallbackToFlashVideo();
-      return false;
-    }
-    this.initMediaControls();
+    if (!video_tags[0].canPlayType || (video_tags[0].canPlayType && video_tags[0].canPlayType('video/mp4') == "no" && video_tags[0].canPlayType('video/ogg') == "no"))
+     video_tags.fallbackToFlashVideo();
+    else 
+      $.each(video_tags, function(i, v){ $(v).show().initMediaControls() });
   },
   
   initMediaControls: function() {
     var tag = $(this).attr('controls', false).mousedown(function(){
-      if(tag[0].paused) { tag.play(); start.hide(); play.hide(); pause.show();}
+      if(tag.paused()) { tag.play(); start.hide(); play.hide(); pause.show();}
       else { tag.pause(); play.show(); pause.hide() };
     });
     var container = tag.parent().css('width', '540px');
@@ -126,23 +186,20 @@ jQuery.fn.extend({
     var pause = controls.find('.pause').mousedown(function(){
       tag.pause(); play.show(); pause.hide();
     });
-    var progress = controls.find('progress').
-      attr('max', tag.
-      attr('duration')).
+    var progress = controls.find('.progress').
       attr('value', 0).
       click(function(e){
         var left = progress.pagePosition().left + 30;
         var ratio = ((e.pageX - left) / progress[0].clientWidth);
-        var pos = ratio * tag[0].duration;
-        tag.attr('currentTime', pos);
+        var pos = ratio * tag.duration();
+        tag.setCurrentTime(pos);
         tag.updateMediaProgress();
       });
     var time = controls.find('.time');
-    setTimeout(function(){ time.html((0).secondsToTime() + ' / ' + (tag.attr('duration') || 0).secondsToTime()) }, 500);
-    var mute = controls.find('.mute').mousedown(function(){
-      tag.attr('muted', !tag.attr('muted'));
-      mute.toggleClass('muted');
-    });
+    
+    if (!tag.attr('flash_id')) setTimeout(function(){ tag.updateTime }, 500); // update the time after loaded
+
+    var mute = controls.find('.mute').mousedown(function(){ tag.mute(); mute.toggleClass('muted'); });
     var volume = controls.find('.volume');
     var volume_buttons = volume.find('button').mousedown(function(e){
       volume_buttons.removeClass('selected');
@@ -155,13 +212,13 @@ jQuery.fn.extend({
         case 'very-loud': var vol = 1.0; break;
       }
       if (tag.attr('muted')) mute.mousedown();
-      tag.attr('volume', vol);
+      tag.setVolume(vol);
       e.stopPropagation();
       return false;
     });
 
-   tag.updateMediaProgress();
-   controls.show();
+    tag.updateMediaProgress();
+    if(!tag.attr('is_flash')) controls.show();
   }
 });
 

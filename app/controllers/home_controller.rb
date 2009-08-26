9af9f463e2def3a715bc46902f6ddde01947d0a3
Tieg Zaharia <tieg.zaharia@gmail.com>
2009-08-26 11:57:32 -0400

    added tag cloud on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
 
     @recent_reviews = Review.claimed.newest(2)
     @recent_episodes = Episode.newest(3)
-    @popular_tags = Tag.all #(:order => "taggings_count DESC")
+    @tags = Tag.all(:order => "taggings_count DESC").sort_by &:rand
 
     @podcast1 = Podcast.find_by_clean_url("COOP") || Podcast.all[0]
     @podcast2 = Podcast.find_by_clean_url("Diggnation") || Podcast.all[1]
